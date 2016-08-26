fc1b6166b66f2308d78c33d5d66c7cd26c2f2ec1
Walter Moreira <walter@waltermoreira.net>
2016-08-26 01:08:13 -0500

    Enable submit only when everything is filled

--- js/init.js
+++ js/init.js
@@ -1,7 +1,40 @@
 (function($){
-  $(function(){
+    $(function(){
 
-    $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav();
+        $('button').addClass('disabled');
+        $('input').change(
+            function() {
+                if (all_filled())
+                    $('button').removeClass('disabled');
+                else
+                    $('button').addClass('disabled');
+            }
+        );
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+function all_filled() {
+    var filled = true;
+    $('input[id="name"], input[id="email"]').each(
+        function() {
+            if ($(this).val() == '')
+                filled = false;
+        }
+    );
+    var fostering = false;
+    $('input[name="fostering"]:checked').each(
+        function() {
+            fostering = true;
+        }
+    );
+    var lifestyle = false;
+    $('input[name="lifestyle"]:checked').each(
+        function() {
+            lifestyle = true;
+        }
+    );
+    return filled && fostering && lifestyle;
+}
